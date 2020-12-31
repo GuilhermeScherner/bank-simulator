eb32a6fb9c074c57a679aa42d48b51e8352dae30
GuilhermeScherner <guibarasuol99@gmail.com>
2020-12-31 15:01:10 -0300

    Implements Transfer

--- transfer/app/main.py
+++ transfer/app/main.py
@@ -1,26 +1,26 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 import pydantic
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Float, DateTime, func
-
+from sqlalchemy import Column, Integer, String, Float
+import sqlalchemy as sla
+from typing import List
 
 app = FastAPI()
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/banksimulator"
+SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/banksimulator"
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, future=True)
 
 Base = declarative_base()
 
 
-class Transfer(Base):
-    __tablename__ = "transfer"
+class Users(Base):
+    __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    value = Column(Float)
     cpf = Column(String)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    balance = Column(Float)
 
 
 class BaseModel(pydantic.BaseModel):
@@ -28,19 +28,84 @@ class BaseModel(pydantic.BaseModel):
         orm_mode = True
 
 
-class TranferRequest(BaseModel):
+class CreateUser(BaseModel):
     name: str
-    value: float
     cpf: str
+    balance: float
+
+
+class ListUsers(BaseModel):
+    users: List[CreateUser]
+
+
+class TransferRequest(BaseModel):
+    cpf_sender: str
+    value: float
+    cpf_receiver: str
+    description: str
+
+
+class UserRequest(BaseModel):
+    name: str
+    cpf: str
+    balance: float
 
 
 class TransferResponse(BaseModel):
     id: int
 
 
-@app.post("/transfer", response_model=TransferResponse)
-async def create_transfer(transfer: TranferRequest) -> TransferResponse:
+class UserResponse(BaseModel):
+    id: int
+
+
+async def get_db():
+    db = AsyncSession(engine)
     try:
-        return {id: 10}
+        yield db
+    finally:
+        await db.close()
+
+
+@app.get("/users", response_model=ListUsers)
+async def get_all_users(db: AsyncSession = Depends(get_db)):
+    qb = sla.select(Users)
+    result = await db.execute(qb)
+    all_result = result.scalars().all()
+    users = list(map(CreateUser.from_orm, all_result))
+    return ListUsers(users=users)
+
+
+@app.post("/users", response_model=UserResponse)
+async def create_user(user: UserRequest, db: AsyncSession = Depends(get_db)):
+    create = Users(**user.dict())
+    db.add(create)
+    await db.commit()
+    await db.refresh(create)
+    return UserResponse.from_orm(create)
+
+
+@app.post("/transfer")
+async def create_transfer(transfer: TransferRequest, db: AsyncSession = Depends(get_db)):
+    try:
+        qb = sla.select(Users).where(Users.cpf == transfer.cpf_receiver)
+        exe = await db.execute(qb)
+        balance_receiver = (exe.scalars().first()).balance
+
+        qb = sla.select(Users).where(Users.cpf == transfer.cpf_sender)
+        exe = await db.execute(qb)
+        balance_sender = (exe.scalars().first()).balance
+
+        qb = sla.update(Users).where(Users.cpf == transfer.cpf_receiver).values(balance=balance_receiver+transfer.value)
+
+        await db.execute(qb)
+
+        qb = sla.update(Users).where(Users.cpf == transfer.cpf_sender).values(balance=balance_sender-transfer.value)
+
+        await db.execute(qb)
+
+        await db.commit()
+        return {"Result": "Success"}
     except:
-        raise HTTPException(status_code=404, detail="ERROR")
+        raise HTTPException(status_code=404, detail="Transition has not success")
+
